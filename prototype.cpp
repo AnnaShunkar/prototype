cfbc8b7557c3836acd744c1027f84f15a43dafee
AnnaShunkar <anna.shunkar4@gmail.com>
2025-10-20 13:09:49 +0300

    Add files via upload

---
+++ prototype.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+// abstract prototype //
+class IMessage {
+public:
+    virtual IMessage* clone() const = 0;
+    virtual void show() const = 0;
+    virtual ~IMessage() = default;
+};
+
+// concrete prototype//
+class HelloMessage : public IMessage {
+private:
+    string text;
+public:
+    HelloMessage(const string& t) : text(t) {}
+
+    IMessage* clone() const override {
+        return new HelloMessage(text);
+    }
+
+    void show() const override {
+        cout << "Message: " << text << endl;
+    }
+};
+
+int main() {
+    HelloMessage original("Hello, world!");
+    IMessage* copy = original.clone();
+
+    original.show();
+    copy->show();
+
+    delete copy;
+    return 0;
+}
